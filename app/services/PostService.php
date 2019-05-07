4cfc52f26ae48515e7beb61a8608eb899b883e4e
cathyjw <jwang4@ualberta.ca>
2019-05-07 16:11:04 -0600

    Services layer, DAL, Factory for Post

---
+++ app/services/PostService.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of PostService
+ *
+ * @author Jie
+ */
+class PostService {
+    //put your code here
+}
